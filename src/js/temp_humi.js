a2cc02234ebb8a9e0a13615fc437c8721ab9b8b3
glonghini <43152762+glonghini@users.noreply.github.com>
2018-09-18 13:12:53 -0300

    Update temp_humi.js

--- src/js/temp_humi.js
+++ src/js/temp_humi.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const temp = '32';
 const humi = '56%';
 
-loadJSON('http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3680/hours/72?token=2e30b5793b0c662d9b901520663bd4d0', climatempoData)
-
-function climatempoData(climatempo_data) {
-  console.log(climatempo_data);
-}
-
-class TempHumi extends React.Component {
+export class TempHumi extends React.Component {
+  componentDidMount() {
+    axios.get('http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3680/hours/72?token=2e30b5793b0c662d9b901520663bd4d0')
+      .then(response => {
+        this.climatempoData = response;
+        console.log(response);
+        console.log(this.climatempoData.data.data[0].date);
+      });
+  }
   render() {
     return (<div>
       <h2>Temperatura e Umidade</h2>
       <h3>Sensores locais</h3>
-      <h4>Temperatura: {climatempo_data.data.date}</h4>
+      <h4>Temperatura: {this.climatempoData}</h4>
       <h4>Umidade: {humi}</h4>
       <h3>Climatempo</h3>
       <h4>Temperatura: </h4>
@@ -24,7 +26,3 @@ class TempHumi extends React.Component {
   );
   }
 }
-
-ReactDOM.render(<TempHumi />, document.getElementById('temp-humi'));
-
-export default TempHumi;
