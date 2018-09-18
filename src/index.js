db5eaf06cc0b3d90aca16cca36151a3a6685d809
glonghini <43152762+glonghini@users.noreply.github.com>
2018-09-18 13:10:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Header from "./js/header";
-import TempHumi from "./js/temp_humi";
+import {Header} from "./js/header.js";
+import {TempHumi} from "./js/temp_humi.js";
 
-ReactDOM.render(React.createElement(
-  'h3',
-  null,
-  'FUNCIONA CARAIO'
-), document.getElementById('meu saco'));
+ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<TempHumi />, document.getElementById('temp-humi'));
