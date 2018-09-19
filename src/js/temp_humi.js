599bf847cb0f4d5b59bde105190336db0e822672
glonghini <43152762+glonghini@users.noreply.github.com>
2018-09-19 11:36:33 -0300

    Update temp_humi.js

--- src/js/temp_humi.js
+++ src/js/temp_humi.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 
+const date = new Date();
+const hour = date.getHours();
+
 export class TempHumi extends React.Component {
+  state = {
+    temperatura_CT: null,
+    umidade_CT: null,
+    temperatura_SEL: null,
+    umidade_SEL: null,
+  }
   componentDidMount() {
-    axios.get('http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3680/hours/72?token=2e30b5793b0c662d9b901520663bd4d0')
-      .then(response => {
-        console.log(response.data);
-        this.temperaturaAgora = response.data.data[];
-        console.log(this.state.climatempoData);
+    axios.get('http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3680/current?token=2e30b5793b0c662d9b901520663bd4d0')
+      .then(response_ct => {
+        console.log(response_ct.data);
+        this.setState(
+          {
+            temperatura_CT: response_ct.data.data.temperature,
+            umidade_CT: response_ct.data.data.humidity,
+          }
+        );
+      });
+    axios.get('http://www.sel.eesc.usp.br/liosensors/json_data.php')
+      .then(response_sel => {
+        console.log(response_sel.data);
+        this.setState(
+          {
+            temperatura_SEL: response_sel.data.lastTemp,
+            umidade_SEL: response_sel.data.lastHum,
+          }
+        );
       });
   }
   render() {
     return (<div>
       <h2>Temperatura e Umidade</h2>
       <h3>Sensores locais</h3>
-      <h4>Temperatura: {this.climatempoData}</h4>
-      <h4>Umidade: {humi}</h4>
-      <ul>{this.state.climatempoData.map(item => <li key={item.date_br}>{item.date}</li>)}</ul>
-      <h4>Temperatura: </h4>
-      <h4>Umidade: </h4>
+      <h4>Temperatura: {this.state.temperatura_SEL} °C</h4>
+      <h4>Umidade: {this.state.umidade_SEL}%</h4>
+      <h2>Climatempo</h2>
+      <h4>Temperatura: {this.state.temperatura_CT} °C</h4>
+      <h4>Umidade: {this.state.umidade_CT}%</h4>
     </div>
   );
   }
