43cd2323ba7865a8ac511931588389bd9d9ae7ea
glonghini <43152762+glonghini@users.noreply.github.com>
2018-09-19 01:52:25 -0300

    Update temp_humi.js

--- src/js/temp_humi.js
+++ src/js/temp_humi.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 
-const temp = '32';
-const humi = '56%';
-
 export class TempHumi extends React.Component {
   componentDidMount() {
     axios.get('http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3680/hours/72?token=2e30b5793b0c662d9b901520663bd4d0')
       .then(response => {
-        this.climatempoData = response;
-        console.log(response);
-        console.log(this.climatempoData.data.data[0].date);
+        console.log(response.data);
+        this.temperaturaAgora = response.data.data[];
+        console.log(this.state.climatempoData);
       });
   }
   render() {
@@ -19,7 +16,7 @@ export class TempHumi extends React.Component {
       <h3>Sensores locais</h3>
       <h4>Temperatura: {this.climatempoData}</h4>
       <h4>Umidade: {humi}</h4>
-      <h3>Climatempo</h3>
+      <ul>{this.state.climatempoData.map(item => <li key={item.date_br}>{item.date}</li>)}</ul>
       <h4>Temperatura: </h4>
       <h4>Umidade: </h4>
     </div>
